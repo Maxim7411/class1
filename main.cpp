d5cfa8da946e7b6703138df571869a2eb8027cf7
Maxim <sokharev.maxim@yandex.ru>
2021-10-26 17:10:56 +0300

    days in range

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int SecondsInMinute(int a)
 {
 	return 60*a;
 }
-void LaterInYear(x1,y1,x2,y2,&x,&y)
+void LaterInYear(int x1,int y1,int x2,int y2,int &x,int &y)
 {
 	if(y1>y2 || (y1==y2 && x1>x2))
 	{
@@ -19,6 +19,17 @@ void LaterInYear(x1,y1,x2,y2,&x,&y)
 		x=x2;
 		y=y2;
 	}
+}
+int DaysInYear(int a)
+{
+	if(IsLeapYear(a)) return 366;
+	 else return 365;
+}
+int DaysInYearRange(int a, int b)
+{
+	int sum=0;
+	for(int i=0;i<b-a;i++) sum+=DaysInYear(i);
+	return sum;
 }
 int main(int argc, char** argv) {
 	return 0;
